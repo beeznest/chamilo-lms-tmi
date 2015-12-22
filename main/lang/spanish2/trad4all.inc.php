1ffa9df7120885a497f1fe6a98639de4348eab56
Yannick Warnier <ywarnier@beeznest.org>
2015-12-22 10:54:44 -0500

    Add custom language

---
+++ main/lang/spanish2/trad4all.inc.php
@@ -0,0 +1,8 @@
+<?php
+$MailCronCourseExpirationReminderSubject="Recordatorio – 1 semana para el fin del curso %s";
+$MailCronCourseExpirationReminderBody="Hola %s,\n\n¡El tiempo vuela cuando aprendes y disfrutas! Ya estamos cerca al final del curso.\n\nEste es solo un pequeño recordatorio de que el curso %s cerrará el %s\n\nAún tienes 1 semana para revisar los recursos, concluir las actividades del curso y/o compartir tus experiencias con la comunidad.\n\n¡Que tengas un buen día!\n\nEquipo Tademi\n\nRío de la Plata 167 Of. 503\nSan Isidro - Lima, Perú \n(511) 221-2721";
+$SocialNetwork="Mi perfil";
+$TabsSocial="Pestaña Mi perfil";
+$SeeCourseInformationAndRequirements="Ver información del curso";
+$Sessions="Cursos disponibles";
+$SignUp="¡Inscríbete!";
