08823f7a67c22237e854d4e23e61be300cc43036
José Loguercio <jloguercio.work@gmail.com>
2015-12-22 13:15:51 -0500

    Nuevos Textos de Correos TMI - Refs #10525

--- main/lang/spanish2/trad4all.inc.php
+++ main/lang/spanish2/trad4all.inc.php
@@ -1,8 +1,21 @@
 <?php
 $MailCronCourseExpirationReminderSubject="Recordatorio – 1 semana para el fin del curso %s";
-$MailCronCourseExpirationReminderBody="Hola %s,\n\n¡El tiempo vuela cuando aprendes y disfrutas! Ya estamos cerca al final del curso.\n\nEste es solo un pequeño recordatorio de que el curso %s cerrará el %s\n\nAún tienes 1 semana para revisar los recursos, concluir las actividades del curso y/o compartir tus experiencias con la comunidad.\n\n¡Que tengas un buen día!\n\nEquipo Tademi\n\nRío de la Plata 167 Of. 503\nSan Isidro - Lima, Perú \n(511) 221-2721";
+$MailCronCourseExpirationReminderBody="Hola %s,\n\n¡El tiempo vuela cuando aprendes y disfrutas! Ya estamos cerca al final del curso.\n\nEste es solo un pequeño recordatorio de que el curso %s cerrará el %s\n\nAún tienes 1 semana para revisar los recursos, concluir las actividades del curso y/o compartir tus experiencias con la comunidad.\n\n¡Que tengas un buen día!\n\n";
 $SocialNetwork="Mi perfil";
 $TabsSocial="Pestaña Mi perfil";
 $SeeCourseInformationAndRequirements="Ver información del curso";
 $Sessions="Cursos disponibles";
 $SignUp="¡Inscríbete!";
+$Hi="Hola";
+$ResetPasswordInstructions="Instrucciones de recuperación de contraseña";
+$Welcome="¡Bienvenido!";
+$MailCronCourseFinishedSubject="¡Fin del curso %s! Gracias por tu participación";
+$MailCronCourseFinishedBody="Hola %s,
+<br><br>
+Gracias por participar en el curso %s. Esperamos que hayas aprendido y disfrutado del curso.
+<br><br>
+Sigue en contacto con la comunidad de tu curso en Foro: %s
+<br><br>
+Puedes ver tu rendimiento a lo largo del curso en la sección Mi Avance.
+<br><br>
+Saludos cordiales,";
